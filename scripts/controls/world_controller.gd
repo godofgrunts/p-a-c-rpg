7e65d7818373e56600e9d5677a05db3c8f7accdc
GodofGrunts <dergottdergrunten@gmail.com>
2019-08-29 22:18:55 -0400

    Added going back to the previous map(s) with escape. (WorldState singleton)

--- scripts/controls/world_controller.gd
+++ scripts/controls/world_controller.gd
@@ -1,20 +1,24 @@
 extends Control
 
 onready var anim_player = $TransAnimations/AnimationPlayer
-var previous_scene = "test"
 
 func _ready() -> void:
 	SignalManager.connect("change_scene", self, "_change_scene")
-	print(("Previous Scene = %s") % previous_scene)
+	if WorldState.previous_scene.size() < 1 or WorldState.previous_scene[-1] != self.filename:
+		WorldState.previous_scene.append(self.filename)
+
 	
 func _change_scene(to_scene) -> void:
 	var packed_scene = load(to_scene)
-	var instance = packed_scene.instance()
-	
+	#var instance = packed_scene.instance()
 	# Change packed scene, pack it, then switch to it
-	instance.previous_scene = self.filename
-	packed_scene.pack(instance)
+	#packed_scene.pack(instance)
 	
 	anim_player.play("FadeToBlack")
 	yield(anim_player, "animation_finished")
 	get_tree().change_scene_to(packed_scene)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel") and WorldState.previous_scene[0] != self.filename:
+		WorldState.previous_scene.pop_back()
+		_change_scene(WorldState.previous_scene[-1])
